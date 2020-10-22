4fcc87462408a28f2c700f29e2e403fef7436e74
Fynn Schur <fynn.schur@web.de>
2020-10-22 17:22:56 +0200

    Fix: Placeholders

--- src/main/java/de/fynn/guild/api/GuildPlaceholder.java
+++ src/main/java/de/fynn/guild/api/GuildPlaceholder.java
@@ -6,24 +6,17 @@ import org.bukkit.entity.Player;
 
 public class GuildPlaceholder {
 
-    public String setPlaceholder(Player player, String placeholder){
+    public String setPlaceholder(Player player, String text){
         if(player==null){
             return null;
+        }else if(text.contains("%guild_name%")){
+            text = text.replace("%guild_name%",Main.getGuildManager().hasGuild(player) ? Main.getGuildManager().getPlayerGuild(player).getGuildName() : "Gildenlos");
+        }else if(text.contains("%guild_members_amount%")){
+            text = text.replace("%guild_members_amount%",Main.getGuildManager().hasGuild(player) ? Main.getGuildManager().getPlayerGuild(player).getMembers().size()+"1" : "0");
+        }else  if(text.contains("%guild_leader%")){
+            text = text.replace("%guild_leader%",Main.getGuildManager().hasGuild(player) ? Bukkit.getPlayer(Main.getGuildManager().getPlayerGuild(player).getLeader()).getDisplayName() : "");
         }
-        switch (placeholder){
-
-            case "%guild_name":
-                return Main.getGuildManager().hasGuild(player) ? Main.getGuildManager().getPlayerGuild(player).getGuildName() : "Gildenlos";
-
-            case "%guild_members_amount":
-                return Main.getGuildManager().hasGuild(player) ? Main.getGuildManager().getPlayerGuild(player).getMembers().size()+"1" : "0";
-
-            case "%guild_leader":
-                return Main.getGuildManager().hasGuild(player) ? Bukkit.getPlayer(Main.getGuildManager().getPlayerGuild(player).getLeader()).getDisplayName() : "";
-
-            default:
-                return null;
-        }
+        return text;
     }
 
 }
